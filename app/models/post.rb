c8678a6236bff43f6b221c0d05151044505d5f53
Chun Yi <8.5by11@gmail.com>
2022-03-15 19:49:49 -0400

    commit tree 86a37a4f31063d77b565eb12bf2e9112441465e9

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,26 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] } 
+    validates :title, exclusion: { in: [ "True Facts" ] }
+
+    validate :clickbaity?
+
+    def clickbaity?
+        if title.to_s.include?("Won't Believe")
+            true
+        elsif title.to_s.include?("Secret")
+            true
+        elsif title.to_s.include?("Top #{0..9}")
+            true
+        elsif title.to_s.include?("Guess")
+            true
+        elsif title.to_s.include?("You Won't Believe These True Facts")
+            false
+        else
+            errors.add(:title, "is not clickbait-y")
+        end
+    end
+
 end
